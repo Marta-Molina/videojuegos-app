f85723a4647d8e63fffa1d172569229a697f3e20
Marta-Molina <martamolinaescalona@hotmail.com>
2026-02-18 13:11:36 +0100

    arreglada ruta repetida que explotaba todo, que cogía rutas antiguas, ya coge las nuevas y no peta

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import AddVideojuego from "./pages/AddVideojuego";
-import ProtectedRoute from "./components/ProtectedRoute";
-import { AuthProvider } from "./context/AuthContext";
-import NavBar from "./components/NavBar";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/add-videojuego"
-            element={<ProtectedRoute><AddVideojuego /></ProtectedRoute>}
-          />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
